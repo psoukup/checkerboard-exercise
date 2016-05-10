82d2c2685a2cd6e8446055c229f80f7cc36bd3b0
p <p@ps-MacBook-Air.local>
2016-05-09 22:15:14 -0500

    got color inside main div

--- script.js
+++ script.js
@@ -11,17 +11,18 @@ $(function() {
 
 
     for (var i = 0; i < 100; i++) {
-    $("div").appendTo(".container");
-
-    $("div").css({
+    $("<div class=interior'></div>").appendTo(".container");
+}
+    $(".interior").css({
       'background-color': 'black',
       'height': '10px',
       'width': '11.1%',
       'float': 'left',
       'border': '2px solid black',
       'padding-bottom': '11.1%',
+      'position': 'relative',
     });
-}
+
     // $('div:nth-child(odd)').css({"background-color", "red"});
     //
     // $('div:nth-child(even)').css({"background-color", "black"});
