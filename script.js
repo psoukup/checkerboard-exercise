4b699261dfd4fb4c604bf064047b385b1bb025d4
p <p@ps-MacBook-Air.local>
2016-05-09 22:29:09 -0500

    board red and black complete - still needs work

--- script.js
+++ script.js
@@ -2,29 +2,33 @@
 
 $(function() {
     $("<div class='container'></div>").appendTo("body");
-    $("body .container").css({
-      "height": "1000px",
-      "width": "1000px",
+    $(".container").css({
+      "height": "960px",
+      "width": "990px",
       "border": "10px solid black",
-      "background-color": "red",
+      "margin": "0px",
+      "overflow": "0"
     })
 
 
-    for (var i = 0; i < 100; i++) {
-    $("<div class=interior'></div>").appendTo(".container");
-}
+    for (var i = 0; i < 72; i++) {
+    $("<div class='interior'></div>").appendTo($(".container"));
+   }
     $(".interior").css({
       'background-color': 'black',
       'height': '10px',
       'width': '11.1%',
       'float': 'left',
-      'border': '2px solid black',
       'padding-bottom': '11.1%',
       'position': 'relative',
     });
 
-    // $('div:nth-child(odd)').css({"background-color", "red"});
-    //
-    // $('div:nth-child(even)').css({"background-color", "black"});
+    $(".interior:nth-child(even)").css({
+      "background-color": "black",
+    })
+
+    $(".interior:nth-child(even)").css({
+      "background-color": "red",
+    })
 
   });
